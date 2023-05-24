054d4915ee8034cd2223fababc70e7ec794c5ec3
davidmedev222 <davidmedev@gmail.com>
2023-05-24 18:06:24 -0300

    fix: change the position of the styles for the font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 import TaskListPage from './pages/task-list/TaskListPage'
 import './styles/reset.css'
 
