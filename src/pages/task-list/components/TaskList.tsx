fc483282ca973179d4e05007f918beabc093d0a4
davidmedev222 <davidmedev@gmail.com>
2023-05-24 18:35:48 -0300

    fix: convert tasks into an array of tasks and map it's contents

--- src/pages/task-list/components/TaskList.tsx
+++ src/pages/task-list/components/TaskList.tsx
@@ -4,76 +4,39 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import ListItemText from '@mui/material/ListItemText'
-import Typography from '@mui/material/Typography'
 
 export default function TaskList() {
+  const tasks = [
+    {
+      id: 1,
+      name: 'Ali Connors',
+      description: " — I'll be in your neighborhood doing errands this…"
+    },
+    {
+      id: 2,
+      name: 'Summer BBQ',
+      description: " — Wish I could come, but I'm out of town this…"
+    },
+    {
+      id: 3,
+      name: 'Oui Oui',
+      description: ' — Do you have Paris recommendations? Have you ever…'
+    }
+  ]
+
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      <ListItem alignItems='center'>
-        <ListItemAvatar>
-          <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
-        </ListItemAvatar>
-        <ListItemText
-          primary='Brunch this weekend?'
-          secondary={
-            <>
-              <Typography
-                sx={{ display: 'inline' }}
-                component='span'
-                variant='body2'
-                color='text.primary'
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </>
-          }
-        />
-      </ListItem>
-      <Divider variant='inset' component='li' />
-      <ListItem alignItems='center'>
-        <ListItemAvatar>
-          <Avatar alt='Travis Howard' src='/static/images/avatar/2.jpg' />
-        </ListItemAvatar>
-        <ListItemText
-          primary='Summer BBQ'
-          secondary={
-            <>
-              <Typography
-                sx={{ display: 'inline' }}
-                component='span'
-                variant='body2'
-                color='text.primary'
-              >
-                to Scott, Alex, Jennifer
-              </Typography>
-              {" — Wish I could come, but I'm out of town this…"}
-            </>
-          }
-        />
-      </ListItem>
-      <Divider variant='inset' component='li' />
-      <ListItem alignItems='center'>
-        <ListItemAvatar>
-          <Avatar alt='Cindy Baker' src='/static/images/avatar/3.jpg' />
-        </ListItemAvatar>
-        <ListItemText
-          primary='Oui Oui'
-          secondary={
-            <>
-              <Typography
-                sx={{ display: 'inline' }}
-                component='span'
-                variant='body2'
-                color='text.primary'
-              >
-                Sandra Adams
-              </Typography>
-              {' — Do you have Paris recommendations? Have you ever…'}
-            </>
-          }
-        />
-      </ListItem>
+      {tasks.map((task, index) => (
+        <>
+          <ListItem key={task.id} alignItems='center'>
+            <ListItemAvatar>
+              <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+            </ListItemAvatar>
+            <ListItemText primary={task.name} secondary={task.description} />
+          </ListItem>
+          {index !== tasks.length - 1 && <Divider variant='inset' component='li' />}
+        </>
+      ))}
     </List>
   )
 }
